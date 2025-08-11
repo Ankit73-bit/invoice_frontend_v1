0b97c1caf899cfb042203a5ba108f9ab2d8cb01b
Ankit Bhuwad <124443636+Ankit73-bit@users.noreply.github.com>
2025-08-11 18:09:11 +0530

    Update
    - Update api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://invoice-backend-v1-jjb0.onrender.com/api",
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://invoice-backend-v1-jjb0.onrender.com/api",
+  // baseURL: "http://localhost:8000/api",
   withCredentials: true, // for cookies
 });
